60f093bafc47beb9351ac6f173ea175c85de4df2
Pawel Suszek <Pawel.Suszek@flir.se>
2017-09-27 23:08:21 +0200

    minor refactoring

---
+++ IdeaExample/src/main/kotlin/intermediate/AndroidLayout.kt
@@ -0,0 +1,13 @@
+package intermediate
+
+//odwolanie bezosrednio do layoutu:
+//text_view_id.text = "aaa"
+//zamiast:
+//val forecastList: RecyclerView = find(R.id.forecast_list)
+//
+//doAsync {
+//// do in background
+//    uiThread {
+//        // do on UI thread
+//    }
+//}
