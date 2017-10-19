d0ad7460d9e2a748870f3bc04f1d8ac8a2abdd15
Pawel Suszek <pawel.suszek2@gmail.com>
2017-10-19 22:16:38 +0200

    added coroutines example

---
+++ IdeaExample/src/main/kotlin/advanced/MyCoroutines.kt
@@ -0,0 +1,43 @@
+package advanced
+
+
+import kotlinx.coroutines.experimental.*
+
+val now = System.currentTimeMillis()
+
+fun corTest(){
+    val v1: Deferred<String> = async { bgTask() }
+
+    myPrint("2")
+    Thread.sleep(3000)
+
+    runBlocking {
+        myPrint("run blocking")
+        val s = v1.await()
+        myPrint("deferred = $s")
+    }
+    myPrint("3")
+
+    // Start a coroutine
+    launch(CommonPool) {
+        myPrint("start coroutine")
+        delay(1000)
+        myPrint("Hello")
+    }
+    myPrint("4")
+
+    Thread.sleep(4000) // wait for 2 seconds
+    myPrint("Stop")
+}
+
+
+fun bgTask(): String  {
+    myPrint(">>bgTask()")
+    Thread.sleep(2000)
+    myPrint("<<bgTask()")
+    return "aa"
+}
+
+fun myPrint(msg: String){
+    println("${System.currentTimeMillis()-now} - $msg")
+}
