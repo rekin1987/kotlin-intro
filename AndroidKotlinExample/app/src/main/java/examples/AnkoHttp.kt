f2b2095a423eda3872fc0507a5c379876ba6cafa
Pawel Suszek <pawel.suszek2@gmail.com>
2017-10-22 22:53:23 +0200

    http request example

--- AndroidKotlinExample/app/src/main/java/examples/AnkoHttp.kt
+++ AndroidKotlinExample/app/src/main/java/examples/AnkoHttp.kt
@@ -1,2 +1,19 @@
 package examples
 
+import java.net.URL
+
+ class MyResult{
+    var id: Int? = null
+    var title: String? = null
+}
+
+private object gson{
+    fun <T> fromJson(str: String, clazz: Class<T>) : T {
+        return 1 as T
+    }
+}
+
+ fun execute(): MyResult {
+    val moviesJsonStr = URL("http://mydomain.com/movies.json").readText()
+    return gson.fromJson(moviesJsonStr, MyResult::class.java)
+}
